7112b5b965447a61fc216fda691e75ab916989d2
Reynard Timotius <91584193+Xhourstoprogram@users.noreply.github.com>
2022-01-12 17:04:02 +0700

    Add files via upload

---
+++ smoothie.js
@@ -0,0 +1,13 @@
+class Smoothie{
+    constructor(ingredients) {
+        const getcost = {
+            total = strawberries + banana + mango + blueberries + raspberries + apple + pineapple
+        } 
+    const getprice = {
+        strawberries = 1.50, banana = 0.50, mango = 2.50, blueberries = 1.00, raspberries = 1.00, apple = 1.75, pineapple = 3.50
+    }
+    const getname = {
+        
+    }   
+}
+}
